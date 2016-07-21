923dd1f7d37d8b78b2b3c0e5eb37dfc1d78ae91b
jakob <qaphka@gmail.com>
2016-07-21 02:02:25 +0200

    spaces

--- export.py
+++ export.py
@@ -9,11 +9,11 @@ import catconv.stabi as sb
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument("source")
-parser.add_argument("target")
-parser.add_argument("-u", "--update", help="overwrite previous results",
+parser.add_argument('source')
+parser.add_argument('target')
+parser.add_argument('-u', '--update', help='overwrite previous results',
                     action="store_true")
-parser.add_argument("-e", "--ext", help="file-extesion", required=False)
+parser.add_argument('-e', '--ext', help='file-extension', required=False)
 args = parser.parse_args()
 
 source = sb.op.normpath(args.source)
@@ -22,14 +22,14 @@ data_dir, target_cat_name = sb.op.split(target)
 
 catalog = sb.load_catalog(source, {'ext': args.ext}, text_box=True, text=True)
 pages = catalog['pages']
-conv = {'rel_path': source}
-catalog['pages'] = map(lambda page: sb.convert_page_path(page, conv), catalog['pages'])
+conversion = {'rel_path': source}
+catalog['pages'] = map(lambda page: sb.convert_page_path(page, conversion), catalog['pages'])
 
 
 fileConfig('logging_config.ini')
 logger = logging.getLogger()
 logger.info('Processing {}'.format(catalog['name']))
-logger.info("Number of pages {}".format( len(pages)))
+logger.info('Number of pages {}'.format(len(pages)))
 
 with open(target, 'wb') as jfile:
     sb.ujson.dump(catalog, jfile, ensure_ascii=False,indent=2, escape_forward_slashes=False)
